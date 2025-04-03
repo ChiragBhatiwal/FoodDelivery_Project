3fde4019d5f3760c00aa57560cec3eacf35c22c7
Chirag Bhatiwal <chiragb8888@gmail.com>
2025-04-03 20:07:29 +0530

    Added Some Functionality In Order Controller

--- server/src/Controllers/Order.Controller.js
+++ server/src/Controllers/Order.Controller.js
@@ -8,21 +8,17 @@ import { ObjectId } from 'mongodb'
 
 //Placing Order For the Buyer With All Necessary Details
 const buyerPlacingOrder = async (itemId, restaurantId, userId, quantity) => {
-
-    if (!itemId) {
-        return "Item Id is Required"
-    }
-
+    try{
     const itemDetails = await itemModel.findById(itemId);
 
     if (!itemDetails) {
-        return "Item Is'nt Fetched"
+        return {success:false,message:"Item Not Found"}
     }
 
     const itemPrice = itemDetails.itemPrice;
 
     if (quantity == null) {
-        return "Qunatity must required"
+        return {success:false,message:"Quantity Can't Be Null"}
     }
 
     const totalBill = quantity * itemPrice;
@@ -30,10 +26,15 @@ const buyerPlacingOrder = async (itemId, restaurantId, userId, quantity) => {
     const orderPlacing = await orderModel.create({ itemDetails: itemId._id, addressId, restaurantId, quantity, userId: userId._id, itemPrice, totalBill });
 
     if (!orderPlacing) {
-        return resp.status(404).send("Something went wrong while submitting data");
+        return {success:false,message:"Failed To Place Order"}
     }
 
-    return "Order Placed Successfully";
+    return {success:true,message:"Order Placed SuccessFully"}
+
+}catch(error)
+{
+    return {success:false,message:"Something Went Wrong",error:error.message}
+}
 }
 
 //Fetching Product Details To Buyer With Hanlding Real Time Qunatity Updation 
@@ -111,154 +112,60 @@ const productDeatilsForBuyer = async (req, resp) => {
     resp.status(200).json({ itemDetails, userDetails });
 }
 
-const orderForDelivery = async (req, resp) => {
-
-    const { restaurantId } = req.body;
-
-    if (!restaurantId) {
-        return resp.status(404).send("Restaurant Id is required");
-    }
-
-    const findOrders = await orderModel.aggregate([
-        {
-            $match: { status: { $in: ["Pending", "Preparing"] } }
-        },
-        {
-            $lookup: {
-                from: "items",
-                localField: "itemDetails",
-                foreignField: "_id",
-                as: "Items"
-            }
-        },
-        {
-            $unwind: "$Items"  // Unwind the "Items" array to flatten the structure
-        },
-        {
-            $lookup: {
-                from: "restaurants",
-                localField: "restaurantId",
-                foreignField: "_id",
-                as: "Restaurant"
-            }
-        },
-        {
-            $unwind: "$Restaurant"  // Unwind the "Restaurant" array to flatten the structure
-        },
-        {
-            $lookup: {
-                from: "locations",
-                localField: "addressId",
-                foreignField: "_id",
-                as: "Address"
-            }
-        },
-        {
-            $unwind: "$Address"  // Unwind the "Address" array to flatten the structure
-        },
-        {
-            $lookup: {
-                from: "users",
-                localField: "userId",
-                foreignField: "_id",
-                as: "User"
-            }
-        },
-        {
-            $unwind: "$User"
-        },
-        {
-            $match: { restaurantId: new ObjectId(restaurantId) }
-        },
-        {
-            $project: {
-                status: 1,
-                orderId: "$_id",  // Project the orderId field (which is the _id in the order collection)
-                _id: 0,  // Exclude the _id field from the output
-                "Items._id": 1,
-                "Items.itemName": 1,
-                itemPrice: 1,
-                totalBill: 1,
-                quantity: 1,
-                "Items.dishType": 1,
-                "Items.itemImage": 1,
-                "Restaurant.restaurantName": 1,
-                "Restaurant.typeOfRestaurant": 1,
-                "Restaurant._id": 1,
-                "Address.Address": 1,
-                "User.username": 1,
-                "User.phoneNumber": 1
-            }
-        }
-    ]);
-
-
-    if (!findOrders) {
-        return resp.status(404).send("Can't Fetched Successfully");
-    }
-
-    return findOrders[0];
-}
-
 //Fetching All The Orders Buyer Bought In Past
 const getBuyAgainOrders = async (user) => {
-    if (!user) {
-        return "Token not Found"
-    }
+    try {
+        const result = await orderModel.aggregate([
+            {
+                $match: {
+                    userId: new ObjectId(user),
+                    status: { $in: ["Delivered"] }
+                }
+            },
+            {
+                $lookup: {
+                    from: "products",
+                    foreignField: "_id",
+                    localField: "productId",
+                    as: "ProductDetails"
+                }
+            },
+            {
+                $addFields: {
+                    productDetails: { $arrayElemAt: ["$ProductDetails", 0] } // Extract first product object
+                }
+            },
+            {
+                $project: {
+                    productId: "$productDetails._id",
+                    productName: "$productDetails.productName",
+                    productImage: "$productDetails.productImage",
+                    productPrice: "$productDetails.productPrice",
+                    totalBill: 1,
+                    quantity: 1,
+                    paymentType: 1,
+                    status: 1,
+                    createdAt: 1
+                }
+            }
+        ]);
 
-
-    const result = await orderModel.aggregate([
-        {
-            $match: {
-                userId: new ObjectId(user._id),
-                status: { $in: ["Delivered"] }
-            }
-        },
-        {
-            $lookup: {
-                from: "products",
-                foreignField: "_id",
-                localField: "productId",
-                as: "ProductDetails"
-            }
-        },
-        {
-            $addFields: {
-                productDetails: { $arrayElemAt: ["$ProductDetails", 0] } // Extract first product object
-            }
-        },
-        {
-            $project: {
-                productId: "$productDetails._id",
-                productName: "$productDetails.productName",
-                productImage: "$productDetails.productImage",
-                productPrice: "$productDetails.productPrice",
-                totalBill: 1,
-                quantity: 1,
-                paymentType: 1,
-                status: 1,
-                createdAt: 1
-            }
+        if (!result) {
+            return { success: false, message: "No Orders Found" }
         }
-    ]);
 
-    if (!result) {
-        return "Not Found Any Orders"; 
+
+        return { success: true, message: "Buy-Again Orders Fetched", result }
+    } catch (error) {
+        return {
+            success: false, meessage: "Something Went Wrong", error: error.message
+        }
     }
-
-
-    return result;
 }
 
 //Fetching All The Orders Which Buyer Orders And In Processing Not Yet Delivered
 const getCurrentOrderedItemList = async (user) => {
-
-
-    if (!user) {
-        return resp.send("Token not Found");
-    }
-
-
+    try{
     const result = await orderModel.aggregate([
         {
             $match: {
@@ -295,10 +202,15 @@ const getCurrentOrderedItemList = async (user) => {
     ]);
 
     if (!result) {
-        return "Error Something Went Wrong"
+        return {success:false,message:"Failed To Fetch Current Orders"}
     }
 
-    return  result 
-    
+    return {success:true,message:"Current Orders Fetched",result}
+
+}catch(error)
+{
+    return {success:false,message:"Something Went Wrong",error:error.message}
 }
-export { buyerPlacingOrder,getBuyAgainOrders,getCurrentOrderedItemList}
+
+}
+export { buyerPlacingOrder, getBuyAgainOrders, getCurrentOrderedItemList }
